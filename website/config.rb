9a093d4668cf0066054e415d7bf19f46945041f2
mgaida <michael.gaida@agoda.com>
2017-10-11 19:37:20 +0700

    bumped version

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.consul.io/"
 
 activate :hashicorp do |h|
   h.name        = "consul"
-  h.version     = "0.9.3-AGODA-2"
+  h.version     = "0.9.3-AGODA-3"
   h.github_slug = "hashicorp/consul"
 end
 
